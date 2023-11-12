dbaa9af0f614b404a9882c0e669daae58213afe4
danwalex <alexisdanwalex@gmail.com>
2023-11-12 14:25:04 +0100

    [-] Suppression de django

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import Agent
-
-class AgentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Agent
-        fields = ['user', 'ip_address', 'public_key']
