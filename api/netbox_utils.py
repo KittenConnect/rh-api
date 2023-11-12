ecaf64c7b9e5f5d4a7deef5abd96b2f1745f9c5f
danwalex <alexisdanwalex@gmail.com>
2023-11-12 10:08:50 +0100

    [+] modified:   api/nebox_utils.py     add function CreateIpinNetbox, checkipinNetbox and generateip this function generate ip address and check if ip exist in netbox before create it

--- api/netbox_utils.py
+++ api/netbox_utils.py
@@ -1,52 +1,76 @@
 import requests
 import json
-import ipaddress
+import random
+
 NETBOX_API_URL = 'https://netbox.360p.kube.kittenconnect.net/api/'
 HEADERS = {
-    'Authorization': 'Token 3f530a0bad7165b7cfcf5686f2154192d5aadc3f',
+    'Authorization': 'Token ',
     'Content-Type': 'application/json',
     'Accept': 'application/json',
 }
+# Vous pouvez ajouter d'autres fonctions utiles ici (spoiler: c'est mieux)
+## Generate Loopback IP address start 2a13:79c0:ffff:fefe::/64 pour faire un /128  en hexa exemple 	2a13:79c0:ffff:fefe::b47d/128
+def generate_short_ipv6_address():
+    prefix = "2a13:79c0:ffff:fefe::/64"
 
-def get_prefix(prefix_id):
-    url = f"{NETBOX_API_URL}ipam/prefixes/{prefix_id}/"
-    
-    response = requests.get(url, headers=HEADERS)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    # Générez un suffixe aléatoire de 4 caractères hexadécimaux pour /128
+    suffix = "".join(random.choice("0123456789abcdef") for _ in range(4))
 
+    # Concaténez le préfixe et le suffixe pour former l'adresse IPv6 complète
+    ipv6_address = f"{prefix[:-4]}:{suffix}/128"
 
-def allocate_subnet(prefix_id, prefix_length, description, role):
-    # Obtenir les informations sur le préfixe IPv6 à partir de son ID
-    prefix_info = get_prefix(prefix_id)
-    
-    if prefix_info:
-        # Construire l'URL pour obtenir les adresses IPv6 disponibles dans le préfixe
-        available_ip_url = f"{NETBOX_API_URL}/ipam/prefixes/{prefix_id}/available-ips/"
-        
-        # Obtenir les adresses IPv6 disponibles dans le préfixe
-        response = requests.get(available_ip_url, headers=HEADERS)
-        
+    return ipv6_address
+
+def checkIpinNetbox(ip_address):
+    params = {
+        "address": ip_address,
+    }
+    apiurl=NETBOX_API_URL+"ipam/ip-addresses/"
+    try:
+        response = requests.get(apiurl, headers=HEADERS, params=params)
+
         if response.status_code == 200:
-            reponse_json = response.json()
-            
-            # Sélectionner la première adresse IPv6 en /128
-            if reponse_json:
-                ipv6_subnet_entry = reponse_json[0]
-                ipv6_subnet = ipv6_subnet_entry["address"]
-                
-                # Obtenir le préfixe /128
-                subnet_128 = ipaddress.IPv6Network(ipv6_subnet, strict=False)
-                
-                # Retourner l'adresse IPv6 en /128 sous forme de chaîne
-                return str(subnet_128)
-    
-    # Si quelque chose ne se passe pas comme prévu ou si le préfixe n'existe pas
-    return None
-def allocate_loopback(prefix_id, description):
-    return allocate_subnet(prefix_id,"128", description, "1")
+            ip_data = response.json()
+            if ip_data["count"] > 0:
+                return True  # L'adresse IPv6 existe déjà dans NetBox
+            else:
+                return False  # L'adresse IPv6 n'existe pas dans NetBox
+        else:
+            print("Erreur lors de la recherche de l'adresse IPv6 dans NetBox.")
+            print(response.text)
+            return False
+    except Exception as e:
+        print(f"Une erreur s'est produite : {str(e)}")
+        return False
 
-# Vous pouvez ajouter d'autres fonctions utiles ici
+def CreateIpinNetbox(ip_address):
+    apiurl=NETBOX_API_URL+"ipam/ip-addresses/"
+    data = {
+        "address": ip_address,
+        "description": "Description de l'adresse IPv6",
+        "custom_fields": {
+            "Pubkey": "Votre clé publique",  
+            "Userid": 5
+        }  
+    }
+
+    try:
+        response = requests.post(apiurl, headers=HEADERS, json=data)
+
+        if response.status_code == 201:
+            print(f"L'adresse IPv6 {ip_address} a été créée avec succès dans NetBox.")
+        else:
+            print("Erreur lors de la création de l'adresse IPv6 dans NetBox.")
+            print(response.text)
+    except Exception as e:
+        print(f"Une erreur s'est produite : {str(e)}")
+
+def generate_or_create_ipv6_address():
+    while True:
+        ipv6_address = generate_short_ipv6_address()
+        if not checkIpinNetbox(ipv6_address):
+            CreateIpinNetbox(ipv6_address)
+            return ipv6_address
+test =generate_or_create_ipv6_address()
+print(test)
+
