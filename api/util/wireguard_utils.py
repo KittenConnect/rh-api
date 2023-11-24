837bee5f2eae530dce0da7487a0ddf6bf9ce7ffc
Romain Neil <contact@romain-neil.fr>
2023-11-24 20:53:33 +0100

    feat: base wireguard utility class

---
+++ api/util/wireguard_utils.py
@@ -0,0 +1,14 @@
+class WireguardUtils:
+
+    def __init__(self):
+        pass
+
+    def get_tunnel_ip(self) -> list:
+        """
+        Retourne la liste des ip qui vont constituer le tunnel
+        :return:
+        """
+        pass
+
+    def etc(self):
+        pass
