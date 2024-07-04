7fd5dc917974ff9f5943c9e115b0f8d7e2622f67
Romain Neil <contact@romain-neil.fr>
2024-07-04 18:00:29 +0200

    feat: base netbox connector

---
+++ model/netbox.go
@@ -0,0 +1,102 @@
+package model
+
+import (
+	"context"
+	"errors"
+	"github.com/netbox-community/go-netbox/v4"
+	"os"
+)
+
+var MachinesSerials map[string]string
+
+var (
+	NetboxVmSerialPrefix string = os.Getenv("NETBOX_VM_SERIAL_FIELD")
+)
+
+// Netbox structure
+// For internal use ONLY !
+// To get an instance, call NewNetbox method
+type Netbox struct {
+	ctx context.Context
+
+	Client *netbox.APIClient
+
+	_isConnected bool
+}
+
+// NewNetbox return a fresh Netbox object
+func NewNetbox() Netbox {
+	nbx := Netbox{
+		ctx:    context.Background(),
+		Client: nil,
+
+		_isConnected: false,
+	}
+
+	return nbx
+}
+
+func (n Netbox) IsConnected() bool {
+	return n._isConnected
+}
+
+func (n Netbox) Connect() error {
+	if n._isConnected {
+		return nil
+	}
+
+	n.Client = netbox.NewAPIClientFor(os.Getenv("NETBOX_API_URL"), os.Getenv("NETBOX_API_KEY"))
+	n._isConnected = true
+
+	return nil
+}
+
+func (n Netbox) CreateVM(serial string) error {
+	if !n._isConnected {
+		return errors.New("netbox is not connected")
+	}
+
+	_ = n.Client.VirtualizationAPI.VirtualizationVirtualMachinesCreate(context.Background())
+
+	//TODO
+	return nil
+}
+
+func (n Netbox) UpdateVM(serial string, conf string) error {
+	if !n._isConnected {
+		return errors.New("netbox is not connected")
+	}
+
+	// Call netbox API with specific serial, then update his settings accordingly
+	_, exist := MachinesSerials[serial]
+	if exist {
+		return nil
+	}
+
+	res, _, err := n.Client.VirtualizationAPI.
+		VirtualizationVirtualMachinesList(n.ctx).
+		//Name([]string{serial}).
+		//Limit(1).
+		Execute()
+	if err != nil {
+		return err
+	}
+
+	var vmId int32
+	var hasFoundVm bool = false
+
+	for _, vm := range res.Results {
+		if vm.CustomFields[NetboxVmSerialPrefix] == serial {
+			vmId = vm.Id
+			hasFoundVm = true
+			break
+		}
+	}
+
+	if !hasFoundVm {
+		//Create VM
+		print(vmId) //TODO
+	}
+
+	return nil
+}
