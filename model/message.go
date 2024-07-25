51e676453c713391936bd14c92d675f026c2b537
Romain Neil <contact@romain-neil.fr>
2024-07-25 17:46:42 +0200

    feat: include machine serial inside message

--- model/message.go
+++ model/message.go
@@ -5,6 +5,7 @@ import "time"
 type Message struct {
 	Hostname  string `json:"hostname"`
 	IpAddress string `json:"ipaddress"`
+	Serial    string `json:"serial"`
 
 	Timestamp time.Time `json:"-"`
 }
