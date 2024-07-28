6b2126cba169ba45dfd9ef291446c880d8067243
Romain Neil <contact@romain-neil.fr>
2024-07-28 17:16:49 +0200

    refactor: rework machine serial compute

--- model/message.go
+++ model/message.go
@@ -1,14 +1,29 @@
 package model
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 type Message struct {
 	Hostname  string `json:"hostname"`
 	IpAddress string `json:"ipaddress"`
-	Serial    string `json:"serial"`
+	serial    string `json:"serial" binding:"optional"`
 
 	//Make following json field optional with default 0
 	FailCount int `json:"failcount" binding:"optional"`
 
 	Timestamp time.Time `json:"-"`
+}
+
+func (m *Message) parseSerial() string {
+	return strings.Join(strings.Split(m.Hostname, "-")[1:], "-")
+}
+
+func (m *Message) GetSerial() string {
+	if m.serial == "" {
+		m.serial = m.parseSerial()
+	}
+
+	return m.serial
 }
