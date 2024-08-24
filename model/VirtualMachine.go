934b8a896c3836761d0132272629500fd653ebfa
Romain Neil <contact@romain-neil.fr>
2024-08-25 00:37:07 +0200

    wip: extract update of vm management ip

--- model/VirtualMachine.go
+++ model/VirtualMachine.go
@@ -21,6 +21,10 @@ type VirtualMachine struct {
 	n            *Netbox
 }
 
+var (
+	mgmtInterfaceName = "mgmt"
+)
+
 func NewVM(n *Netbox, msg Message) *VirtualMachine {
 	vm := &VirtualMachine{
 		n:        n,
@@ -60,6 +64,10 @@ func (vm *VirtualMachine) Create(msg Message) (*virtualization.VirtualizationVir
 	return vm.n.Client.Virtualization.VirtualizationVirtualMachinesCreate(params, nil)
 }
 
+func (vm *VirtualMachine) CreateOrUpdate(msg Message) {
+	//
+}
+
 // Update vm infos to netbox
 func (vm *VirtualMachine) Update() error {
 	data := vm.Get()
@@ -94,7 +102,53 @@ func (vm *VirtualMachine) GetInterfaces(name string) (*virtualization.Virtualiza
 	return interfaces, nil
 }
 
-func (vm *VirtualMachine) CreateInterface(n *Netbox, ifName string) (*virtualization.VirtualizationInterfacesCreateCreated, error) {
+func (vm *VirtualMachine) GetInterfaceByID(id int64) (*models.VMInterface, error) {
+	vmId := strconv.FormatInt(vm.NetboxId, 10)
+	interfaceId := strconv.FormatInt(id, 10)
+
+	ipIfParam := &virtualization.VirtualizationInterfacesListParams{
+		VirtualMachineID: &vmId,
+		ID:               &interfaceId,
+	}
+	i, err := vm.n.Client.Virtualization.
+		VirtualizationInterfacesList(ipIfParam.WithTimeout(vm.n.GetDefaultTimeout()), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error listing virtual machine interfaces: %w", err)
+	}
+
+	if *i.Payload.Count != 1 {
+		return nil, fmt.Errorf("error listing virtual machine interfaces: expected 1 item, got %d", i.Payload.Count)
+	}
+
+	return i.Payload.Results[0], nil
+}
+
+func (vm *VirtualMachine) GetManagementInterface() (*models.VMInterface, error) {
+	vmId := strconv.FormatInt(vm.NetboxId, 10)
+
+	ipIfParam := &virtualization.VirtualizationInterfacesListParams{
+		VirtualMachineID: &vmId,
+		Name:             &mgmtInterfaceName,
+	}
+	in, err := vm.n.Client.Virtualization.
+		VirtualizationInterfacesList(ipIfParam.WithTimeout(vm.n.GetDefaultTimeout()), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error listing virtual machine interfaces: %w", err)
+	}
+
+	//If there are no management interface, create it
+	if *in.Payload.Count == 0 {
+		mgmtInterface, err := vm.CreateInterface("mgmt")
+		if err != nil {
+			return nil, fmt.Errorf("error creating virtual machine interface: %w", err)
+		}
+
+		return mgmtInterface.Payload, nil
+	}
+
+	return in.Payload.Results[0], nil
+}
+
 func (vm *VirtualMachine) CreateInterface(ifName string) (*virtualization.VirtualizationInterfacesCreateCreated, error) {
 	ifParam := models.WritableVMInterface{
 		Name:    &ifName,
@@ -158,4 +212,122 @@ func (vm *VirtualMachine) CreateIP(n *Netbox, address string, status string, lin
 	}
 
 	return res, nil
+}
+
+func (vm *VirtualMachine) UpdateManagementIP(msg Message) error {
+	//Get vm management interface
+	itf, err := vm.GetManagementInterface()
+	if err != nil {
+		return fmt.Errorf("error getting interfaces: %w", err)
+	}
+
+	objectType := "virtualization.vminterface"
+
+	//Update management interface with latest IP
+	err = vm.UpdateInterfaceIP(msg.IpAddress, itf.ID, objectType)
+	if err != nil {
+		return err
+	}
+
+	var mgmtInterfaceId = strconv.FormatInt(itf.ID, 10)
+	params := ipam.NewIpamIPAddressesListParams()
+	params.SetVminterfaceID(&mgmtInterfaceId)
+
+	result, err := vm.n.Client.Ipam.IpamIPAddressesList(params, nil)
+	if err != nil {
+		return fmt.Errorf("error listing ip addresses: %w", err)
+	}
+
+	var ipCount = result.Payload.Count
+	util.Info("There are actually %s IP(s) associated with the management interface", strconv.FormatInt(*ipCount, 10))
+
+	if *ipCount > 1 {
+		return fmt.Errorf("there are more than one management ip linked to the management interface")
+	}
+
+	if *ipCount == 1 {
+		ip := result.Payload.Results[0]
+		if *ip.Address == msg.IpAddress {
+			//Nothing to do
+			return nil
+		}
+
+		// 4. The management IP changed, so :
+		// - unlink the old ip and interface
+		// - set the new ip to the interface
+
+		oldIpUpdatePrams := models.WritableIPAddress{
+			Address:            &msg.IpAddress,
+			AssignedObjectType: nil,
+			AssignedObjectID:   nil,
+		}
+
+		paramUnlinkOldIp := ipam.
+			NewIpamIPAddressesPartialUpdateParams().
+			WithID(ip.ID).
+			WithData(&oldIpUpdatePrams).
+			WithTimeout(vm.n.GetDefaultTimeout())
+		_, err = vm.n.Client.Ipam.IpamIPAddressesPartialUpdate(paramUnlinkOldIp, nil)
+		if err != nil {
+			return fmt.Errorf("error updating management ip addresses of VM #%d: %w", vm.NetboxId, err)
+		}
+
+		util.Success("Successfully updated management ip addresses of VM #%d with new IP: %s", vm.NetboxId, msg.IpAddress)
+		return nil
+	}
+
+	// 5. No existing IP, but verify that she doesn't already exist in the netbox
+	ipSearchParams := ipam.NewIpamIPAddressesListParams()
+	ipSearchParams.Q = &msg.IpAddress
+	result, err = vm.n.Client.Ipam.IpamIPAddressesList(ipSearchParams, nil)
+	if err != nil {
+		return fmt.Errorf("error listing existing ip addresses: %w", err)
+	}
+
+	existingIpCount := result.Payload.Count
+	newIpAddrId := int64(0)
+	if *existingIpCount == 0 {
+		util.Info("There is no IP registered in the netbox. Create him.")
+		var ipType = "virtualization.vminterface"
+		newIp := &ipam.IpamIPAddressesCreateParams{
+			Data: &models.WritableIPAddress{
+				Address:            &msg.IpAddress,
+				AssignedObjectID:   &itf.ID,
+				AssignedObjectType: &ipType,
+			},
+		}
+		r, err := vm.n.Client.Ipam.IpamIPAddressesCreate(newIp.WithTimeout(vm.n.GetDefaultTimeout()), nil)
+		if err != nil {
+			return fmt.Errorf("error creating ip address: %w", err)
+		}
+
+		newIpAddrId = r.Payload.ID
+	} else {
+		newIpAddrId = result.Payload.Results[0].ID
+	}
+
+	var ipType = "virtualization.vminterface"
+
+	ip := vm.n.getIpAddress(msg.IpAddress)
+	ip.ID = newIpAddrId
+	ip.AssignedObjectID = &itf.ID
+	ip.AssignedObjectType = &ipType
+
+	ifUpdateParam := &ipam.IpamIPAddressesPartialUpdateParams{
+		Data: ip,
+	}
+	_, err = vm.n.Client.Ipam.IpamIPAddressesPartialUpdate(ifUpdateParam.WithTimeout(vm.n.GetDefaultTimeout()), nil)
+	if err != nil {
+		return fmt.Errorf("error updating ip address: %w", err)
+	}
+
+	return nil
+}
+
+func (vm *VirtualMachine) Exists(hostname string, serial string) (bool, int64, error) {
+	if vm.NetboxId <= 0 && vm.n == nil {
+		return false, 0, nil
+	}
+
+	return vm.n.VmExists(hostname, serial)
 }
